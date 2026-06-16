ba4f89634aa2f7d6eaa2f012304005d41e82bb1b
Devanshu Garg <mrdevanshugarg@gmail.com>
2026-06-16 17:39:40 +0530

    coins change

--- DP/3. DP On Subsequences/20_CoinsChange.cpp
+++ DP/3. DP On Subsequences/20_CoinsChange.cpp
@@ -5,62 +5,34 @@ using namespace std;
 //! https://leetcode.com/problems/coin-change/
 
 // ***************************************MEMOIZATION*************************************
-class Solution {
-    public:
-    int solve(int i, int amount, vector<int>& coins, vector<vector<int>> &dp) {
-        //Base Case
-        if(amount == 0) return 0;
-        if(i < 0) return 1e8;
-        
-        if(dp[i][amount] != -1) return dp[i][amount];
-        
-        //pick: take the same coin again (unbounded knapsack)
-        int pick = 1e8;
-        if(amount - coins[i] >= 0)
-        pick = 1 + solve(i, amount - coins[i], coins, dp);
-        
-        // notPick: move to the previous coin
-        int notPick = solve(i-1, amount, coins, dp);
-        
-        return dp[i][amount] = min(pick, notPick);
-    }
-
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans = solve(n-1, amount, coins, dp);
-        return (ans >= 1e8) ? -1 : ans;
-    }
-};
-
-// ***************************************TABULATION*************************************
 
 class Solution {
 public:
+    vector<vector<int>> dp;
+    int solve(int i, int amount, vector<int>& coins) {
+        // Base Case
+        if(amount == 0) return 0;
+        if(i < 0) return 1e8;
+
+        if(dp[i][amount] != -1) return dp[i][amount];
+        
+        // Not pick: move to the previous coin
+        int ans  = solve(i-1, amount, coins);
+
+        // Pick: take the same coin again (unbounded knapsack)
+        if(coins[i] <= amount)  
+            ans = min(ans, 1 + solve(i, amount - coins[i], coins));
+        
+
+        return dp[i][amount] = ans;
+    }
+
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, 1e8));
+        dp.assign(n, vector<int>(amount+1, -1));
 
-        //Base Case
-        for(int x = 0; x <= amount; x++) {
-            dp[0][x] = (x % coins[0] == 0) ? x/coins[0] : 1e8; 
-        }
-
-        for(int i = 1; i < n; i++) {
-            for(int x = 0; x <= amount; x++) {
-                int pick = 1e8;
-                if(x - coins[i] >= 0)
-                    pick = 1 + dp[i][x - coins[i]];
-
-                // notPick: move to the previous coin
-                int notPick = dp[i-1][x];
-                
-                dp[i][x] = min(pick, notPick);
-            }
-        }
-
-        int ans = dp[n-1][amount];
-        return (ans >= 1e8) ? -1 : ans;
+        int ans = solve(n-1, amount, coins);
+        return (ans >= 1e8) ? -1: ans;
     }
 };
 
